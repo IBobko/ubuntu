6458d0ff28ec6658fb5121217873330b933d1f5e
IBobko <iibobko@gmail.com>
2024-04-29 01:51:24 +0000

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -2,5 +2,5 @@
 gsettings set org.gnome.shell favorite-apps "$(echo $(gsettings get org.gnome.shell favorite-apps) | sed "s/\]/, 'google-chrome.desktop'\]/")"
 
 # Commands to configure keyboard layouts through gsettings
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]'
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]"
 gsettings set org.gnome.desktop.input-sources xkb-options "['grp:alt_shift_toggle']"
